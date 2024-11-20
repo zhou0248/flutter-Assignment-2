4c975682284afde8eef7e80a9b26d0df90933767
Aiting Zhou <zhou0248@algonquinlive.com>
2024-11-20 18:34:22 -0500

    set up project

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Navigation App"),
+      ),
+      body: Center(
+        child: Column(),
+      ),
+    );
+  }
+}
